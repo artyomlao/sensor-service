8166a38ea7e471c10679ad3dc806bf69a9ca54a7
Artyom <artem.lepesha@mail.ru>
2023-11-12 02:20:21 +0300

    init 2

---
+++ src/main/java/sensor/SensorServiceApplication.java
@@ -0,0 +1,13 @@
+package sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorServiceApplication.class, args);
+	}
+
+}
